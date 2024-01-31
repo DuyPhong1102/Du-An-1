7cbcb2c99f53897c42b2098995e48942dc364dcd
Duy Phong <easylmvp@gmail.com>
2024-01-31 20:52:46 +0700

    first commit

---
+++ app/library/admin/admin.library.php
@@ -0,0 +1,15 @@
+
+
+<!-- Font-icon css-->
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+
+<link rel="stylesheet" href="<?php echo _WEB_ROOT; ?>/app/library/node_modules/boxicons/css/boxicons.min.css">
+<link rel="stylesheet" href="<?php echo _WEB_ROOT; ?>/app/library/node_modules/toastify-js/src/toastify.css">
+<script defer type="text/javascript" src="<?php echo _WEB_ROOT; ?>/app/library/node_modules/toastify-js/src/toastify.js"></script>
+
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<script defer src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
